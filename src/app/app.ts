e7c3c1625d1f3dd366c4ad4d7e0755063c29371b
Panagiotis Soukiouroglou <pansouk@gmail.com>
2026-01-16 18:56:42 +0200

    feat: restructure app with header component and styling updates
    - Added a new standalone Header component with its own HTML and CSS.
    - Updated app component to use the new Header component.
    - Introduced global styles for body and HTML elements.
    - Added new assets including logos and updated angular.json to include asset paths.
    - Removed placeholder content from app.html and replaced it with the Header component.
    - Enhanced styling for header and main application layout.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Header } from "./header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('first-angular-app');
-}
+export class App {}
