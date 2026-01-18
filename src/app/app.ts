f83475f49ab16ec03ffb5eac0c02914f2c35b655
Panagiotis Soukiouroglou <pansouk@gmail.com>
2026-01-18 12:09:46 +0200

    feat: implement user selection feature with dummy user data and styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from "./header/header";
+import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header],
+  imports: [Header, User ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
