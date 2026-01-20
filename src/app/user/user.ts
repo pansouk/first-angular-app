b33a8440a1776714b38ab9af937fdc0cf00fa7fb
Panagiotis Soukiouroglou <pansouk@gmail.com>
2026-01-20 15:52:07 +0200

    feat: refactor user component to use a single user input object for better data handling

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -3,6 +3,12 @@ import { DUMMY_USERS } from '../../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
+interface TheUser {
+  id: string
+  avatar: string
+  name: string
+}
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -11,17 +17,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 })
 export class User {
 
-  @Input({ required: true }) id !: string
-  @Input({ required: true }) avatar  !: string
-  @Input({ required: true }) name !: string
+
+  @Input({ required: true }) user !: TheUser
+
 
   @Output() select = new EventEmitter<string>()
 
   get imagePath() {
-    return 'assets/users/' + this.avatar
+    return 'assets/users/' + this.user.avatar
   }
 
   onSelectUser() {
-    this.select.emit(this.id)
+    this.select.emit(this.user.id)
   }
 }
