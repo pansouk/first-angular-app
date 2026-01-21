e07aa7241af99a218e7e7c9db0ee0ad721af8f0c
Panagiotis Soukiouroglou <pansouk@gmail.com>
2026-01-21 16:56:40 +0200

    feat: update task component to accept taskItem input and display task details in tasks list

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface TaskItem {
+  id: string
+  userId: string
+  title: string
+  summary: string
+  dueDate: string
+}
 
 @Component({
   selector: 'app-task',
@@ -7,5 +15,5 @@ import { Component } from '@angular/core';
   styleUrl: './task.css',
 })
 export class Task {
-
+  @Input({ required: true }) taskItem!: TaskItem
 }
