87e2df3730ee99cab18c3df748637292fd758ca2
Panagiotis Soukiouroglou <pansouk@gmail.com>
2026-01-23 11:08:13 +0200

    feat: add new task component and integrate with tasks list

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Task } from "./task/task";
 import { dummyTasks } from '../../dummy-tasks';
+import { NewTask } from "./new-task/new-task";
 
 @Component({
   selector: 'app-tasks',
-  imports: [Task],
+  imports: [Task, NewTask],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
@@ -13,6 +14,7 @@ export class Tasks {
   tasks = dummyTasks
   @Input({ required: true }) userId !: string
   @Input({ required: true }) name!: string
+  isAddingTask = false
 
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId)
@@ -20,5 +22,9 @@ export class Tasks {
 
   onTaskComplete(taskId: string) {
     this.tasks = this.tasks.filter(task => task.id !== taskId);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
   }
 }
